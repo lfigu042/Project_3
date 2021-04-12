dd59f9621e417cb575d460da7e945f8093431bc4
Laura Figueroa <lfigu042@fiu.edu>
2021-04-12 15:18:53 -0400

    class graph and interface graphinterface added

---
+++ GraphInterface.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface GraphInterface{
+    public void addEdge(int v, int w);
+    public void removeEdge(int v, int w);
+    public int[] findAdjacencyVertices(int v);
+    public String toString();
+}
