7a5a044dc5545dd651ef82d00a8b013758c32a33
Laura Figueroa <lfigu042@fiu.edu>
2021-04-18 19:30:06 -0400

    fixed start and end node stay the same (circuit) fixed repetition of the same node within the path. Correct shortest path is found! Avoid code running 2+ times Graphically output path
    To-Do:
    test more node.
    do extra credit

--- GraphDisplay.java
+++ GraphDisplay.java
@@ -10,14 +10,16 @@ public class GraphDisplay extends JPanel {
     int height = 50; //height of nodes
     int labelX = 20;
     int labelY = 30;
-    int fontSize = 20;
+    int labelsFontSize = 20;
+    int weightsFontSize = 12;
+    String weight;
 
     Graph inputGraph = new Graph();
     private final int[][] coordinates = inputGraph.getCoordinateMatrix(); //coordinates
     private final int numVertices = inputGraph.getVerticesNumber(); //vertices
     private final int[][] edges = inputGraph.getEdgeMatrix(); //edges matrix with weight
-    private final int[] path = inputGraph.getShortestPath(); //shortest hamiltonian cycle
-    private final int distance = inputGraph.getShortestDistance();
+    private final int[] localSearchPath = inputGraph.getShortestPath(); //shortest hamiltonian cycle
+    private final int localSearchDistance = inputGraph.getShortestDistance();
 
     public GraphDisplay(int W, int H){ //CONSTRUCTOR, get window measurements from FrameDisplay
         screen_W = W;
@@ -25,8 +27,6 @@ public class GraphDisplay extends JPanel {
         offSet = screen_W/2;
         System.out.println("screen dimensions: "+ screen_W+ " x "+ screen_H);
     }
-
-
 
     public void paint(Graphics g) {
         System.out.println("paint() ran");
@@ -44,27 +44,47 @@ public class GraphDisplay extends JPanel {
         g.setColor(Color.GREEN);
         g.drawLine(x1+labelX , y1, x2+labelX , y2 );
     }
+    public void drawPathWeight(int s, int e, Graphics g){
+        System.out.println("drawPathWeight() ran\n");
+        System.out.printf("start %d, end %d",s,e);
+        int x,y;
+        weight = String.valueOf(edges[s][e]); //weight value of an edge
+
+
+//        //draw weight in the middle point of each edge
+        x = labelX + ((coordinates[s][0] + coordinates[e][0]) / 2);
+        y = 10 + ((coordinates[s][1] + coordinates[e][1]) / 2);
+
+        g.setColor(Color.DARK_GRAY);
+        g.setFont(new Font("TimesRoman", Font.BOLD, weightsFontSize));
+        g.drawString(weight, x, y);
+    }
     public void drawPathInfo( Graphics g){
-        int l = path.length;
+        int l = localSearchPath.length;
         int indent = 25;
         int lines = screen_H-100;
-        String output = "";
+        String outputLocalSearch = "";
 
         System.out.println("drawPathInfo() ran");
-        g.setFont(new Font("TimesRoman", Font.PLAIN, fontSize-5));
+        g.setFont(new Font("TimesRoman", Font.PLAIN, labelsFontSize -5));
 
         for (int i = 0; i < l ; i++) {
             if(i == l - 1) {
-                 output += path[i];
+                 outputLocalSearch += localSearchPath[i];
             }else {
-                drawShortestPath(path[i],path[i+1],g);
-                output += path[i] + " -> ";
+                drawShortestPath(localSearchPath[i], localSearchPath[i+1],g);
+                drawPathWeight(localSearchPath[i], localSearchPath[i+1],g);
+                outputLocalSearch += localSearchPath[i] + " -> ";
             }
         }
         g.setColor(Color.black);
-        g.drawString("Shortest path: ", indent, lines);
-        g.drawString(output, indent, lines += 20);
-        g.drawString("Total distance: " + distance , indent, lines += 20);
+
+        g.drawString("Shortest path with local search: ", indent, lines);
+        g.drawString("Shortest path with exhaustive search: ", indent + offSet, lines);
+
+        g.drawString(outputLocalSearch, indent, lines += 20);
+        g.drawString("Total distance: " + localSearchDistance, indent, lines + 20); //local search
+        g.drawString("Total distance: TBD" , indent + offSet, lines + 20); //exhaustive search
 
     }
     public void drawEdges(Graphics g){
@@ -88,7 +108,7 @@ public class GraphDisplay extends JPanel {
     public void drawVertices( Graphics g){
         System.out.println("drawVertices() ran");
         int x,y;
-        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, fontSize));
+        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, labelsFontSize));
 
         for (int i = 0; i < numVertices; i++){
             x = coordinates[i][0];
